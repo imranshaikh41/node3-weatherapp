88e106fbb70da3caf21611c4e9e06ba2a7fe463a
Imran Shaikh <imranshaikh@techechelons.net>
2019-09-16 15:12:47 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+console.log('Client side js is called')
+
+fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+
+response.json().then((data)=>{
+    console.log(data)
+})
+
+})
+
+
+
+const weatherForm=document.querySelector('form')
+const search=document.querySelector('input')
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit',(e)=>
+{
+    e.preventDefault()
+    const loc=search.value
+    messageOne.textContent="Loading.."
+    messageTwo.textContent=''
+console.log('Inside onther lister'+loc)
+
+fetch('http://localhost:3000/weather?address='+loc).then((response)=>{
+
+        response.json().then((data)=>{
+        if(data.error)
+        {
+            messageOne.textContent=data.error
+        }
+        else
+        {
+            messageOne.textContent= data.Location
+            messageTwo.textContent=data.forecast 
+           
+
+        }
+           
+            })
+        })
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require('request')
+const forecast = (lat, long, callback) => {
+
+    const url = 'https://api.darksky.net/forecast/c1833fd811e5c8c3e925fda4230dfe47/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?units=si'
+
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to process the request', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            //const data = JSON.parse(response.body)
+            const current = body.currently;
+            const forecast = 
+                // temp: body.currently.temperature,
+                // percip: body.currently.precipProbability,
+                // summary: body.currently.summary
+
+               "it is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + " % chance of rain. "+ body.currently.summary
+          
+            // console.log("it is currently " + current.temperature + " degrees out. There is " + current.precipProbability + " % chance of rain")
+            callback(undefined, forecast)
+        }
+    })
+}
+
+module.exports = forecast;
